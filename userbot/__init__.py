f2f217de6a634db537f98d41206bb506a77dfd43
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-09-02 05:43:27 +0300

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -21,23 +21,29 @@ from .helpers.functions.musictool import *
 from .helpers.utils.utils import runasync
 from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-__version__ = "3.1.1"
+__version__ = "3.3.0"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "ZThon <https://github.com/Zed-Thon/ZelZal>"
-__copyright__ = f"ZThon Copyright (C) 2021 - 2022  {__author__}"
+__author__ = "CatUserBot <https://github.com/TgCatUB/catuserbot>"
+__copyright__ = f"Copyright (C) 2020 - 2023  {__author__}"
 
-zedub.version = __version__
-zedub.tgbot.version = __version__
-LOGS = logging.getLogger("سهى")
-bot = zedub
+catub.version = __version__
+catub.tgbot.version = __version__
+LOGS = logging.getLogger("CatUserbot")
+bot = catub
 
 StartTime = time.time()
-zedversion = "3.1.1"
+catversion = "3.3.0"
 
-if Config.UPSTREAM_REPO == "King":
-    UPSTREAM_REPO_URL = "https://github.com/asaaqa/King"
-else:
-    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+
+def close_connection(*_):
+    print("Closing Userbot connection.")
+    runasync(catub.disconnect())
+    sys.exit(143)
+
+
+signal.signal(signal.SIGTERM, close_connection)
+
+UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
 
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
@@ -49,7 +55,7 @@ if Config.PRIVATE_GROUP_BOT_API_ID == 0:
         Config.BOTLOG = True
 else:
     if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
-        Config.BOTLOG_CHATID = int(f"-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+        Config.BOTLOG_CHATID = int(f"-{str(Config.PRIVATE_GROUP_BOT_API_ID)}")
     else:
         Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
     Config.BOTLOG = True
@@ -60,7 +66,7 @@ if Config.PM_LOGGER_GROUP_ID == 0:
     else:
         Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
 elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
-    Config.PM_LOGGER_GROUP_ID = int(f"-" + str(Config.PM_LOGGER_GROUP_ID))
+    Config.PM_LOGGER_GROUP_ID = int(f"-{str(Config.PM_LOGGER_GROUP_ID)}")
 
 try:
     if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
